56cc6ed80604f25a9a55d64c87c2b64a121d761d
Kobe McKee <kobe.mckee@gmail.com>
2019-10-08 10:38:17 -0600

    Revised iterative sorting

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -23,14 +23,15 @@ def bubble_sort( arr ):
     n = len(arr)
 
     #loop through all elements in the array
-    for i in range(n):
-        #Last i elements are already in place
-        for j in range(0, n - i - 1):
-            #loop through the array from 0 to n - i - 1
-            #Compare each element with its neighbor
-            if arr[j] > arr[j + 1]:
-                #Swap if true
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    has_swapped = True
+    while has_swapped:
+        has_swapped = False
+        for i in range(n - 1):
+            #Compare each element to its neighbor
+            if arr[i] > arr[i+1]:
+                #Swap them
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                has_swapped = True
 
     return arr
 
